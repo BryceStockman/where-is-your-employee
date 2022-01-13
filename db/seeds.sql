b47340bc7fc25aa91fafbf9dc243bb6524e58750
BryceStockman <65779581+BryceStockman@users.noreply.github.com>
2022-01-13 12:49:25 -0700

    initial setup

---
+++ db/seeds.sql
@@ -0,0 +1,32 @@
+INSERT INTO employee
+  (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Ronald', 'Firbank', 1, 1),
+  ('Piers', 'Gaveston', 2, 0),
+  ('Charles', 'LeRoi', 2, 0),
+  ('Katherine', 'Mansfield', 1, 1),
+  ('Dora', 'Carrington', 3, 0),
+  ('Molly', 'Ringwald', 3, 0),
+  ('Jimmy', 'Fallon', 3, 0),
+  ('Virginia', 'Woolf', 1, 1),
+  ('Edward', 'Bellamy', 4, 0),
+  ('Montague', 'Summers', 4, 0),
+  ('Octavia', 'Butler', 4, 0),
+  ('Unica', 'Zurn', 4, 0);
+
+INSERT INTO role
+  (title, salary, department_id)
+VALUES
+  ('Manager', 150000.00, 1),
+  ('Account Manager', 100000.00, 1),
+  ('Account Specialist', 80000.00, 1),
+  ('Account Coordinator', 50000.00, 1),
+
+INSERT INTO department
+  (name)
+VALUES
+  ('Social'),
+  ('SEO'),
+  ('PPC'),
+  ('Email'),
+
