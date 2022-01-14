ba9124c8691fe967f7eab25f068e04a6a9dd0cfe
BryceStockman <65779581+BryceStockman@users.noreply.github.com>
2022-01-14 16:38:11 -0700

    prompts for all but updateEmployee completed

--- server.js
+++ server.js
@@ -1,24 +1,153 @@
-const express = require('express');
+// const express = require('express');
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+// const apiRoutes = require('./routes/apiRoutes');
+// const htmlRoutes = require('./routes/htmlRoutes');
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
 // Use apiRoutes
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+// app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes);
 
 const Employee = require('./lib/Employee');
 
 const employeeData = [];
+const departmentData = [];
+const roleData = [];
 
-const promptEmployee = () => {
+// LOOK AT THE CHALLENGE SNAPSHOTS TO ADJUST THE INQUIRER PROMPTS
+const promptUser = () => {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'toDo',
+        message: 'What would you like to do? (Required)',
+        choices: [
+          'Add Department',
+          'Add Role',
+          'Add Employee',
+          "Update Employee's Role",
+          'Finish',
+        ],
+        validate: (toDoInput) => {
+          if (toDoInput) {
+            return true;
+          } else {
+            console.log('Please enter what you would like to do');
+            return false;
+          }
+        },
+      },
+    ])
+    .then((info) => {
+      const toDo = info.toDo;
+      if (toDo === 'Add Department') {
+        promptDepartment();
+      } else if (toDo === 'Add Role') {
+        promptRole();
+      } else if (toDo === 'Add Employee') {
+        promptEmployee();
+      } else if (toDo === "Update Employee's Role") {
+        promptUpdateEmployee();
+      } else {
+        console.log(employeeData);
+        console.log(departmentData);
+        console.log(roleData);
+      }
+    });
+};
+
+const promptDepartment = () => {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'department',
+        message: 'What is the name of the department? (Required)',
+        validate: (departmentInput) => {
+          if (departmentInput) {
+            return true;
+          } else {
+            console.log('Please enter what department you would like to add');
+            return false;
+          }
+        },
+      },
+      {
+        type: 'confirm',
+        name: 'addDepartment',
+        message: 'Would you like to add another department? (Required)',
+        validate: (addDepartmentInput) => {
+          if (addDepartmentInput) {
+            return true;
+          } else {
+            console.log('Please confirm');
+            return false;
+          }
+        },
+      },
+    ])
+    .then((info) => {
+      const { department } = info;
+      departmentData.push(department);
+
+      if (info.addDepartment) {
+        promptDepartment();
+      } else {
+        promptUser();
+      }
+    });
+};
+
+const promptRole = () => {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'role',
+        message: 'What role you would like to add? (Required)',
+        validate: (roleInput) => {
+          if (roleInput) {
+            return true;
+          } else {
+            console.log('Please enter what role you would like to add');
+            return false;
+          }
+        },
+      },
+      {
+        type: 'confirm',
+        name: 'addRole',
+        message: 'Would you like to add another role? (Required)',
+        validate: (addRoleInput) => {
+          if (addRoleInput) {
+            return true;
+          } else {
+            console.log('Please confirm');
+            return false;
+          }
+        },
+      },
+    ])
+    .then((info) => {
+      const { role } = info;
+      roleData.push(role);
+
+      if (info.addRole) {
+        promptRole();
+      } else {
+        promptUser();
+      }
+    });
+};
+
+function promptEmployee() {
   inquirer
     .prompt([
       {
@@ -47,7 +176,6 @@ const promptEmployee = () => {
           }
         },
       },
-      // THIS SHOULD BE A LIST OF ROLES FOR THE USER TO CHOOSE
       {
         type: 'number',
         name: 'roleId',
@@ -61,16 +189,15 @@ const promptEmployee = () => {
           }
         },
       },
-      // THIS SHOULD BE A INPUT ASKING WHO IS THE MANAGER
       {
-        type: 'confirm',
-        name: 'managerId',
-        message: "What is the team employee's manager id? (Required)",
-        validate: (managerIdInput) => {
-          if (managerIdInput) {
+        type: 'input',
+        name: 'manager',
+        message: "Who is this employee's manager? (Required)",
+        validate: (managerInput) => {
+          if (managerInput) {
             return true;
           } else {
-            console.log("Please enter the employee's manager id");
+            console.log('Please confirm');
             return false;
           }
         },
@@ -92,24 +219,86 @@ const promptEmployee = () => {
       },
     ])
     .then((info) => {
-      console.log(info);
       // QUESTION: HOW DO I CONVERT THE BOOLEAN TRUE TO THE NUMBER MYSQL IS LOOKING FOR?
-      const { firstName, lastName, roleId, managerId } = info;
-      const employee = new Employee(firstName, lastName, roleId, managerId);
-      console.log(employee);
+      const { firstName, lastName, roleId, manager } = info;
+      const employee = new Employee(firstName, lastName, roleId, manager);
       employeeData.push(employee);
 
       if (info.confirmAddEmployee) {
         promptEmployee();
       } else {
-        console.log('moving on, need to decide what to do here');
+        promptUser();
       }
     });
-};
-// promptEmployee();
+}
 
+function promptUpdateEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'updateEmployee',
+        message: "Which employee's role do you want to update? (Required)",
+        choices: [
+          // QUESTION: CAN I INSERT DATA IN HERE, IF SO, HOW?
+        ],
+        validate: (updateEmployeeInput) => {
+          if (updateEmployeeInput) {
+            return true;
+          } else {
+            console.log('Please enter what you would like to do');
+            return false;
+          }
+        },
+      },
+      {
+        type: 'input',
+        name: 'updateEmployeeRole',
+        message: "What is the employee's role? (Required)",
+        validate: (updateRoleInput) => {
+          if (updateRoleInput) {
+            return true;
+          } else {
+            console.log("Please enter the employee's role");
+            return false;
+          }
+        },
+      },
+      {
+        type: 'confirm',
+        name: 'updateAnotherEmployee',
+        message: "Would you like to update another employee's role? (Required)",
+        validate: (anotherEmployeeInput) => {
+          if (anotherEmployeeInput) {
+            return true;
+          } else {
+            console.log('Please confirm');
+            return false;
+          }
+        },
+      },
+    ])
+    .then((info) => {
+      // QUESTION: I DON'T REALLY KNOW WHAT TO DO HERE
+      const { updateEmployee, updateRole } = info;
+      const updatedEmployee = new Employee(
+        firstName,
+        lastName,
+        roleId,
+        managerId
+      );
+      employeeData.push(employee);
 
+      if (info.updateAnotherEmployee) {
+        promptUpdateEmployee();
+      } else {
+        promptUser();
+      }
+    });
+}
 
-app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
-});
+promptUser();
+
+// app.listen(PORT, () => {
+//   console.log(`API server now on port ${PORT}!`);
+// });
